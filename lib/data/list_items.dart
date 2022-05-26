94e3328630554f02fe1afb2b57f1c1fd5d573521
Dauka12 <93545836+Dauka12@users.noreply.github.com>
2022-05-27 03:17:12 +0600

    Add files via upload

---
+++ lib/data/list_items.dart
@@ -0,0 +1,25 @@
+import 'package:pomodoro/model/list_item.dart';
+
+final List<ListItem> listItems = [
+  ListItem(
+    title: 'Sport',
+    urlImage: 'https://i.pinimg.com/originals/6a/87/b0/6a87b06ee4f2c9ff739f1a4eaa901785.jpg'),
+    ListItem(
+    title: 'Study',
+    urlImage: 'https://w7.pngwing.com/pngs/41/862/png-transparent-student-higher-education-study-skills-learning-products-people-logo-teacher.png'),
+    ListItem(
+    title: 'Clean',
+    urlImage: 'https://p.kindpng.com/picc/s/49-499434_black-and-white-cleaning-lady-clipart-cleaning-lady.png'),
+    ListItem(
+    title: 'Wash',
+    urlImage: 'https://www.pngfind.com/pngs/m/63-637820_png-file-svg-take-a-bath-icon-transparent.png'),
+    ListItem(
+    title: 'Play game',
+    urlImage: 'https://elements-cover-images-0.imgix.net/b3893586-aa8c-42f0-a415-923581b11157?auto=compress%2Cformat&fit=max&w=900&s=18b918c7fe4870f4db73268da6529f93'),
+    ListItem(
+    title: 'Sleep',
+    urlImage: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFM2pLa7rUhoQAV9qUpM0HVRbca-3zoFpC4w&usqp=CAU'),
+    ListItem(
+    title: 'Eat',
+    urlImage: 'https://image.shutterstock.com/image-photo/image-260nw-1166908729.jpg'),
+];
